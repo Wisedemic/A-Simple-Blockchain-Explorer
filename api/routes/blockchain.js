e8566f568361b0fc63f40b118575a286bd9f3343
Wisedemic <navarrete.tristan@gmail.com>
2018-03-28 05:47:09 -0400

    Comment update. Added levelDB to grab the blockchain.

--- api/routes/blockchain.js
+++ api/routes/blockchain.js
@@ -1,7 +1,11 @@
-var express = require('express');
-var chain = express.Router();
+// LevelDB
+let level = require('level');
 
-let blockchain
+// Define a new Express Router
+let express = require('express');
+let chain = express.Router();
+
+let BlockchainDB = level('./blockchain');
 
 chain.get('/', function(req, res, next) {
 	res.json({"yay": "woo"});
