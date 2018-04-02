cb3be5536515427f8083f622c5c5b72c9785c9a2
Wisedemic <navarrete.tristan@gmail.com>
2018-04-02 02:34:57 -0400

    Removed a console.log (not needed)

--- src/components/Block.js
+++ src/components/Block.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 export default class Block extends Component {
 	constructor(props) {
 		super(props);
-		console.log(props);
 		this.state = props.data;
 	}
 
