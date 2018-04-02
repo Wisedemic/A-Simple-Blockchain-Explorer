0bdefd41daf3485b776f3bed803c66b22abfe90a
Wisedemic <navarrete.tristan@gmail.com>
2018-04-02 02:32:21 -0400

    Updated naming conventions. /add route now returns the chain data so the view can be updated in 1 request.

--- api/routes/blockchain.js
+++ api/routes/blockchain.js
@@ -3,14 +3,14 @@ let level = require('level-party');
 
 // Define a new Express Router
 let express = require('express');
-let chain = express.Router();
+let BlockchainRouter = express.Router();
 
 let Block = require ('../blockchain/block.js');
 let Blockchain = require('../blockchain/blockchain.js');
 
 let BlockchainDB = level('./blockchain', {valueEncoding: 'json'});
 
-chain.post('/', function(req, res, next) {
+BlockchainRouter.post('/', function(req, res, next) {
 	BlockchainDB.get('blockchain', function(err, chain) {
 		if (err) console.log(err);
 		if (chain) {
@@ -21,42 +21,46 @@ chain.post('/', function(req, res, next) {
 	});
 });
 
-chain.put('/add', function(req, res, next){
+BlockchainRouter.put('/add', function(req, res, next){
 	//sanitize input
 	//------------
 
 	//------------
-	BlockchainDB.get('blockchain', function(err, chain){
-		if(err){
+	BlockchainDB.get('blockchain', function(err, chain) {
+		if(err) {
 			res.status(500);
 			res.send({error: err});
 			return console.log(err);
 		}
-		if(!chain){
+
+		if(!chain) {
 			res.status(500);
 			res.send({error: 'blockchain wasnt found'});
 			return console.log('blockchain wasnt found!');
 		}
-		
+
 		const latestBlock = chain[chain.length-1];
+
 		const newBlock = new Block(
-			latestBlock.index+1, 
+			latestBlock.index+1,
 			Date.now(),
 			{message: `Block ${latestBlock.index+1} - ${req.body.message}`},
 			latestBlock.hash
 		);
+
 		chain.push(newBlock);
+
 		BlockchainDB.put('blockchain', chain, function(err){
-			if(err){
+			if(err) {
 				res.status(500);
 				res.send({error: 'problem pushing blockchain to database'});
 				return console.log(err);
 			}
 			res.status(200);
-			res.end();
+			res.send(chain);
 			return console.log('added block to blockchain!');
 		});
 	});
 });
 
-module.exports = chain;
+module.exports = BlockchainRouter;
